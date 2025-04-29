5dd8cc6d5493cda5a5975fcede2319b286f3a710
sydni wright <sydniw@udel.edu>
2025-04-29 15:55:18 -0400

    testing connection to main

---
+++ DFS.java
@@ -0,0 +1,10 @@
+public class DFS {
+    public void run(){
+        System.out.println("----Depth First Search----");
+
+        //show the list of nodes we will be using
+        
+        //recursively traverse the tree in the proper order according to DFS
+    }
+
+}
